d69860edbb2da20cd249ffeb6deef83dc091841f
John Atkins <johnatkins1999@yahoo.com>
2017-07-08 20:04:12 -1000

    new

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 const doc = document;
+const mainDiv = doc.getElementsByClassName('mainDiv');
+console.log(mainDiv);
 let req = makeXHRReq('GET', getStories, 'https://www.reddit.com/r/TheWayWeWere.json');
 
 function makeXHRReq( method, listenerFunction, url ) {
@@ -14,13 +16,30 @@ function getStories() {
   let myStories = obj.data.children;
   console.log(myStories);
   for (var i = 0; i < myStories.length; i++) {
+
     let subDiv = doc.createElement('div');
     subDiv.setAttribute('class', 'story');
 
     let image = doc.createElement('img');
+    image.setAttribute('class', 'thumbs')
     image.setAttribute('src', myStories[i].data.url);
-    console.log(image);
 
+    let caption = doc.createElement('div');
+    caption.setAttribute('class', 'caption');
+    console.log(myStories[i].data.title);
+    caption.innerHTML = myStories[i].data.title;
+
+    let subhead = doc.createElement('div');
+    subhead.setAttribute('class', 'subhead');
+
+    let date = Number(myStories[i].data.created);
+    console.log(date);
+    subhead.innerHTML = 'by ' + myStories[i].data.author + ' &bull; ' + Date(date) + ' &bull; ' +  myStories[i].data.view_count + ' views';
+
+    subDiv.appendChild(image);
+    subDiv.appendChild(caption);
+    subDiv.appendChild(subhead);
+    mainDiv[0].appendChild(subDiv);
 
 
 
